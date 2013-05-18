e819759209bfb1af7c7f1f974037dbbfbe404d54
ohtysk <ohtysk@gmail.com>
2013-05-18 17:24:13 +0900

    add get url function

--- FeedItems.hs
+++ FeedItems.hs
@@ -1,8 +1,11 @@
-module FeedItems (getUpdatedItems, saveUpdated, lastUpdated, getItemDateString) where
+module FeedItems (getUpdatedItems, saveUpdated, lastUpdated, getItemDateString, getUrlFromItem) where
 
 import Text.Feed.Import (parseFeedString)
-import Text.Feed.Types (Item)
+import Text.Feed.Types (Item(..))
 import Text.Feed.Query (feedItems, getItemDate)
+import Text.Atom.Feed (linkHref, Entry(..))
+import qualified Text.RSS.Syntax as R
+import qualified Text.RSS1.Syntax as R1
 import Data.Maybe (fromJust)
 import System.Directory (doesFileExist)
 
@@ -40,3 +43,10 @@ getUpdatedItems :: Maybe String -> String -> [Item]
 getUpdatedItems Nothing     feed = updatedItems "0" $ parseItems feed
 getUpdatedItems (Just time) feed = updatedItems time $ parseItems feed
 
+getUrlFromItem :: Item -> String
+getUrlFromItem (AtomItem entry) = linkHref $ head $ entryLinks entry
+getUrlFromItem (RSSItem ritem) = case R.rssItemLink ritem of
+  Just url -> url
+  _ -> ""
+getUrlFromItem (RSS1Item item) = R1.itemURI item
+getUrlFromItem _ = ""
